99ac58b8f257d1569b0dce45c2298870925309b8
plamen-kolev <pkolev22@gmail.com>
2025-10-07 13:01:35 +0200

    patched to use default screen

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,8 @@ cd /workspaces/zmk
 
 # ;/workspaces/modules/parent/zmk-drv2605-driver;/workspaces/modules/parent/zmk-output-behavior-listener;
 # export modules=-DZMK_EXTRA_MODULES="/workspaces/modules/parent/zmk-keyboard-sway;/workspaces/modules/parent/cirque-input-module"
-export MODULES=-DZMK_EXTRA_MODULES="/workspaces/modules/parent/zmk-keyboard-sway;/workspaces/modules/parent/nice-view-gem;"
+# export MODULES=-DZMK_EXTRA_MODULES="/workspaces/modules/parent/zmk-keyboard-sway;/workspaces/modules/parent/nice-view-gem;"
+export MODULES=-DZMK_EXTRA_MODULES="/workspaces/modules/parent/zmk-keyboard-sway;"
 # export MODULES=-DZMK_EXTRA_MODULES="/workspaces/modules/parent/nice-view-gem;"
 export ENVIRONMENT=""
 export SNIPPETS="--snippet studio-rpc-usb-uart"
@@ -27,7 +28,7 @@ west build $PRISTINE \
     $ENVIRONMENT \
     -s /workspaces/zmk/app \
     -d build/sway/left -b nice_nano_v2 -- \
-    -DSHIELD="sway_left nice_view_gem" \
+    -DSHIELD="sway_left nice_view" \
     -DZMK_CONFIG="$USER_FOLDER/config" $MODULES
 
 cp /workspaces/zmk/build/sway/left/zephyr/zmk.uf2 $USER_FOLDER/build/sway-left.uf2
@@ -39,7 +40,7 @@ west build $PRISTINE \
     -s /workspaces/zmk/app \
     -d build/sway/right \
     -b nice_nano_v2 -- \
-    -DSHIELD="sway_right nice_view_gem" \
+    -DSHIELD="sway_right nice_view" \
     -DZMK_CONFIG="$USER_FOLDER/config" $MODULES
 
 cp /workspaces/zmk/build/sway/right/zephyr/zmk.uf2 $USER_FOLDER/build/sway-right.uf2
